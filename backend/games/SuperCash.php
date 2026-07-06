7b8073c808d6a336c6f417578b9ad4ac2c0a99bd
Kevin Heise <kevin.heise.91@gmail.com>
2026-07-06 14:43:19 -0500

    Adding clarifying comments to games and updating project roadmap.

--- backend/games/SuperCash.php
+++ backend/games/SuperCash.php
@@ -12,13 +12,13 @@ class SuperCash implements JsonSerializable {
 	private $panels = array();
 
 
-	private $lowOdd   = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19]; // 0,9
+	private $lowOdd   = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19]; // indexes: 0-9
 
-	private $lowEven  = [ 2,  4,  6,  8, 10, 12, 14, 16, 18, 20]; // 0,9
+	private $lowEven  = [ 2,  4,  6,  8, 10, 12, 14, 16, 18, 20]; // indexes: 0-9
 
-	private $highOdd  = [21, 23, 25, 27, 29, 31, 33, 35, 37, 39]; // 0,9
+	private $highOdd  = [21, 23, 25, 27, 29, 31, 33, 35, 37, 39]; // indexes: 0-9
 
-	private $highEven = [22, 24, 26, 28, 30, 32, 34, 36, 38];     // 0,8
+	private $highEven = [22, 24, 26, 28, 30, 32, 34, 36, 38];     // indexes: 0-8
 
 
 	private $pattern1 = [
