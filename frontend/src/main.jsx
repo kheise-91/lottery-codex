ed5028dec3353a34a958b5543f5144394ce2b974
Kevin Heise <kevin.heise.91@gmail.com>
2026-07-15 20:07:32 -0500

    [Task-107] Wrap App with GameProvider and integrate into main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import { GameProvider } from './contexts/GameContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GameProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GameProvider>
   </StrictMode>,
 )
