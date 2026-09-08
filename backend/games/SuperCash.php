d457f88d706372b79764c84f13b73d0f71fd539d
Kevin Heise <kevin.heise.91@gmail.com>
2026-09-07 19:59:06 -0500

    [Task-189] Load live drawings in SuperCash and Megabucks getHistory()

--- backend/games/SuperCash.php
+++ backend/games/SuperCash.php
@@ -66,6 +66,7 @@ class SuperCash implements GameInterface, \JsonSerializable
      */
     public function getHistory(): array
     {
+        $this->loadPreviousDrawings();
         return $this->getPreviousDrawings();
     }
 
