1c9c599ec2c01a54d0cf5997516c49561daa65cf
Kevin Heise <kevin.heise.91@gmail.com>
2026-09-16 23:19:04 -0500

    [Task-208] Remove dead BrowserRouter import in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/layout/Layout'
 import Dashboard from './pages/Dashboard'
 import GamePage from './pages/GamePage'
